751d798a9bb189ec19436364c259f94165ff8917
Samphilowe <samphilowe@outlook.com>
2025-12-12 01:20:43 -0400

    First commit
    Basic boilerplate sdl2 cpp

---
+++ Game/main.cpp
@@ -0,0 +1,37 @@
+#include <SDL2/SDL.h>
+#include <iostream>
+
+
+
+int main() {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "SDL init error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("Game", SDL_WINDOWPOS_CENTERED,
+                                          SDL_WINDOWPOS_CENTERED, 800, 600, 0);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    bool running = true;
+    SDL_Event event;
+
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) running = false;
+        }
+
+        SDL_SetRenderDrawColor(renderer, 255,255,255, 255);
+        SDL_RenderClear(renderer);
+
+        // Game rendering goes here
+
+        SDL_RenderPresent(renderer);
+        SDL_Delay(16); // ~60 FPS
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}   
